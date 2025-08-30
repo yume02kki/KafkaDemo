fd973e4cc73856b80f75c781dc5c7a1978285fee
ben <beneshel3@gmail.com>
2025-08-30 18:01:23 +0300

    refactor dir

--- PersonSender/PersonSender/Kafka/Config.cs
+++
@@ -1,8 +0,0 @@
-using Confluent.Kafka;
-
-namespace PersonSender;
-
-public class ConfigData
-{
-    public static Config Config { get; private set; } = new ProducerConfig { BootstrapServers = "localhost:9092" };
-}
