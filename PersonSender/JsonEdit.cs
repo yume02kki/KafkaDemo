d6d0cb06653ad397dea6e566a41a844cec23355d
ben <beneshel3@gmail.com>
2025-08-30 18:15:21 +0300

    refactor dir

--- PersonSender/JsonEdit.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json;
-
-namespace PersonSender;
-
-public static class JsonEdit
-{
-    public static string Serialize<T>(T obj) => JsonSerializer.Serialize(obj);
-    public static T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json)!;
-}
